e48e8ef5caef21bcd5801c1b233aff119ad27b26
caliskanbi <109751987+caliskanbi@users.noreply.github.com>
2025-06-03 15:28:56 +0200

    ex3 files aber mit error

---
+++ exercise_3_acceleration_techniques/configparser.cpp
@@ -0,0 +1,107 @@
+#include "configparser.h"
+#include <fstream>
+#include <sstream>
+#include <regex>
+#include <iostream>
+
+Config ConfigParser::parse(const std::string& filename) {
+    Config config;
+    std::ifstream infile(filename);
+    std::string line;
+
+    std::regex number_regex(R"([+-]?\d*\.?\d+)");
+    std::regex vector_regex(R"(\(\s*([+-]?\d*\.?\d+),\s*([+-]?\d*\.?\d+),\s*([+-]?\d*\.?\d+)\))");
+
+    std::vector<Vector3> positions, velocities;
+    std::vector<float> masses;
+
+    while (std::getline(infile, line)) {
+        if (line.find("num_time_step") != std::string::npos) {
+            config.num_time_step = std::stoi(line.substr(line.find("=") + 1));
+        } else if (line.find("time_step_length") != std::string::npos) {
+            config.time_step_length = std::stod(line.substr(line.find("=") + 1));
+        } else if (line.find("sigma") != std::string::npos) {
+            config.sigma = std::stod(line.substr(line.find("=") + 1));
+        } else if (line.find("epsilon") != std::string::npos) {
+            config.epsilon = std::stod(line.substr(line.find("=") + 1));
+        } else if (line.find("particle_num") != std::string::npos) {
+            config.particle_num = std::stoi(line.substr(line.find("=") + 1));
+        } else if (line.find("LSYS") != std::string::npos) {
+            config.LSYS = std::stoi(line.substr(line.find("=") + 1));
+        } else if (line.find("particle_positions") != std::string::npos) {
+            while (std::getline(infile, line) && line.find("};") == std::string::npos) {
+                std::smatch match;
+                if (std::regex_search(line, match, vector_regex)) {
+                    positions.push_back({
+                        std::stod(match[1]),
+                        std::stod(match[2]),
+                        std::stod(match[3])
+                    });
+                }
+            }
+        } else if (line.find("particle_velocities") != std::string::npos) {
+            while (std::getline(infile, line) && line.find("};") == std::string::npos) {
+                std::smatch match;
+                if (std::regex_search(line, match, vector_regex)) {
+                    velocities.push_back({
+                        std::stod(match[1]),
+                        std::stod(match[2]),
+                        std::stod(match[3])
+                    });
+                }
+            }
+        } else if (line.find("particle_masses") != std::string::npos) {
+            while (std::getline(infile, line) && line.find("};") == std::string::npos) {
+                std::smatch match;
+                if (std::regex_search(line, match, number_regex)) {
+                    masses.push_back(std::stod(match[0]));
+                }
+            }
+        }
+    }
+
+    // Zusammensetzen der Partikel
+    size_t n = std::min({positions.size(), velocities.size(), masses.size()});
+    for (size_t i = 0; i < n; ++i) {
+        Particle p;
+        p.position = positions[i];
+        p.velocity = velocities[i];
+        p.mass = masses[i];
+        p.force = {0, 0, 0};         
+        p.acceleration = {0, 0, 0};  
+        config.particles.push_back(p);
+    }
+
+    return config;
+}
+
+// Ausgabeoperatoren
+
+std::ostream& operator<<(std::ostream& os, const Vector3& vec) {
+    os << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Particle& particle) {
+    os << "  Position: " << particle.position << "\n"
+       << "  Velocity: " << particle.velocity << "\n"
+       << "  Force: " << particle.force << "\n"
+       << "  Acceleration: " << particle.acceleration << "\n"
+       << "  Mass: " << particle.mass;
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Config& config) {
+    os << "Time Steps: " << config.num_time_step << "\n";
+    os << "Time Step Length: " << config.time_step_length << "\n";
+    os << "Sigma: " << config.sigma << "\n";
+    os << "Epsilon: " << config.epsilon << "\n";
+    os << "LSYS: " << config.LSYS<< "\n";
+    os << "Particle Count: " << config.particle_num << "\n";
+
+    for (size_t i = 0; i < config.particles.size(); ++i) {
+        os << "Particle " << i + 1 << ":\n" << config.particles[i] << "\n";
+    }
+
+    return os;
+}
---
+++ exercise_3_acceleration_techniques/configparser.h
@@ -0,0 +1,44 @@
+#ifndef CONFIGPARSER_H
+#define CONFIGPARSER_H
+
+#include <string>
+#include <vector>
+#include <ostream>
+
+struct Vector3 {
+    double x;
+    double y;
+    double z;
+
+    Vector3(double x = 0,double y = 0,double z = 0) : x(x), y(y), z(z) {}
+};
+
+struct Particle {
+    Vector3 position;
+    Vector3 velocity;
+    double mass;
+    Vector3 force;
+    Vector3 acceleration;
+};
+
+struct Config {
+    int num_time_step = 0;
+    double time_step_length = 0;
+    double sigma = 0.0f;
+    double epsilon = 0.0f;
+    int particle_num = 0;
+    std::vector<Particle> particles;
+    int LSYS = 0;
+};
+
+class ConfigParser {
+public:
+    static Config parse(const std::string& filename);
+};
+
+// Stream-Ausgaben
+std::ostream& operator<<(std::ostream& os, const Vector3& vec);
+std::ostream& operator<<(std::ostream& os, const Particle& particle);
+std::ostream& operator<<(std::ostream& os, const Config& config);
+
+#endif // CONFIGPARSER_H
